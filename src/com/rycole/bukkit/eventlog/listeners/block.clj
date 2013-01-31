10e2e504c694b1464c176a74d6e103e5bf5df7a2
Ryan Cole <rcole@ilstech.com>
2013-01-31 14:03:44 -0600

    added some more events to log

--- src/com/rycole/bukkit/eventlog/listeners/block.clj
+++ src/com/rycole/bukkit/eventlog/listeners/block.clj
@@ -2,9 +2,17 @@
   (:require [clj-mixpanel.core :as mixpanel])
   (:gen-class :name com.rycole.bukkit.eventlog.listeners.block.BlockListener
               :implements [org.bukkit.event.Listener]
-              :methods [[^{org.bukkit.event.EventHandler true} onBlockBreak [org.bukkit.event.block.BlockBreakEvent] void]]))
+              :methods [[^{org.bukkit.event.EventHandler true} onBlockBreak [org.bukkit.event.block.BlockBreakEvent] void]
+                        [^{org.bukkit.event.EventHandler true} onBlockPlace [org.bukkit.event.block.BlockPlaceEvent] void]]))
 
 (def mixpanel-token "aca99527d3f43f494dd29c0c924db8ee")
 
 (defn -onBlockBreak [this evnt]
-  (mixpanel/notify mixpanel-token "Block Broken" {:distinct-id (.getPlayerListName (.getPlayer evnt))}))
+  (mixpanel/notify mixpanel-token "Block Broken" {:distinct-id (.getPlayerListName (.getPlayer evnt))
+                                                  :player-name (.getPlayerListName (.getPlayer evnt))
+                                                  :block-type (.toString (.getType (.getBlock evnt)))}))
+
+(defn -onBlockPlace [this evnt]
+  (mixpanel/notify mixpanel-token "Block Placed" {:distinct-id (.getPlayerListName (.getPlayer evnt))
+                                                  :player-name (.getPlayerListName (.getPlayer evnt))
+                                                  :block-type (.toString (.getType (.getBlockPlaced evnt)))}))
--- src/com/rycole/bukkit/eventlog/listeners/player.clj
+++ src/com/rycole/bukkit/eventlog/listeners/player.clj
@@ -1,7 +1,16 @@
 (ns com.rycole.bukkit.eventlog.listeners.player
+  (:require [clj-mixpanel.core :as mixpanel])
   (:gen-class :name com.rycole.bukkit.eventlog.listeners.player.PlayerListener
               :implements [org.bukkit.event.Listener]
-              :methods [[^{org.bukkit.event.EventHandler true} onPlayerLoggedIn [org.bukkit.event.player.PlayerLoginEvent] void]]))
+              :methods [[^{org.bukkit.event.EventHandler true} onPlayerJoin [org.bukkit.event.player.PlayerJoinEvent] void]
+                        [^{org.bukkit.event.EventHandler true} onPlayerQuit [org.bukkit.event.player.PlayerQuitEvent] void]]))
 
-(defn -onPlayerLoggedIn [this evnt]
-  (.info (org.bukkit.Bukkit/getLogger) "PLAYER LOGGED IN"))
+(def mixpanel-token "aca99527d3f43f494dd29c0c924db8ee")
+
+(defn -onPlayerJoin [this evnt]
+  (mixpanel/notify mixpanel-token "Player Joined" {:distinct-id (.getPlayerListName (.getPlayer evnt))
+                                                   :player-name (.getPlayerListName (.getPlayer evnt))}))
+
+(defn -onPlayerQuit [this evnt]
+  (mixpanel/notify mixpanel-token "Player Quit" {:distinct-id (.getPlayerListName (.getPlayer evnt))
+                                                 :player-name (.getPlayerListName (.getPlayer evnt))}))
