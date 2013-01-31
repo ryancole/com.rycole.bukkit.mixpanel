5c9e07b3bce3549112d319a86b338e5eb25af2f9
Ryan Cole <ryan@rycole.com>
2013-01-30 22:31:33 -0600

    guess ill try piping this data to mixpanel

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :repositories [["bukkit.release" "http://repo.bukkit.org/content/repositories/releases"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.bukkit/bukkit "1.4.7-R0.1"]
-                 [com.ashafa/clutch "0.4.0-RC1"]]
+                 [clj-mixpanel "0.0.1-SNAPSHOT"]]
   :filespecs [{:type :path :path "src\\plugin.yml"}]
   :aot :all)
--- src/com/rycole/bukkit/eventlog/listeners/block.clj
+++ src/com/rycole/bukkit/eventlog/listeners/block.clj
@@ -1,7 +1,10 @@
 (ns com.rycole.bukkit.eventlog.listeners.block
+  (:require [clj-mixpanel.core :as mixpanel])
   (:gen-class :name com.rycole.bukkit.eventlog.listeners.block.BlockListener
               :implements [org.bukkit.event.Listener]
               :methods [[^{org.bukkit.event.EventHandler true} onBlockBreak [org.bukkit.event.block.BlockBreakEvent] void]]))
 
+(def mixpanel-token "aca99527d3f43f494dd29c0c924db8ee")
+
 (defn -onBlockBreak [this evnt]
-  (.info (org.bukkit.Bukkit/getLogger) "BLOCK BORKEN NOO"))
+  (mixpanel/notify mixpanel-token "Block Broken" {:distinct-id (.getPlayerListName (.getPlayer evnt))}))
